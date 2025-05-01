1b87944c0b012df2ae94d86ef44b3d928afae2fd
prittk <114107523+prittk@users.noreply.github.com>
2025-05-01 19:28:55 -0400

    Added a button

--- gui.py
+++ gui.py
@@ -1,26 +1,42 @@
 import tkinter as tk
 import ImageGraber
 from PIL import ImageGrab, Image, ImageTk
+import time
+
 
 def main_Gui():
     ##start main gui
     root = tk.Tk()
+
     root.title("AutoClicker")
     root.configure(background="grey")
     root.minsize(400,400)
     
+    
     #grab screenshot of clicked area and pack for tk
-    screenshot = ImageGraber.grabScreenshot()
-    screenshot = Image.fromarray(screenshot)
-    image = ImageTk.PhotoImage(screenshot)    
     
+    screenshotButton = tk.Button(root, text="Get Cickable", command =lambda: screenshot(root))
+    screenshotButton.pack()
     
-    tk.Label(root,image=image).pack()
+    #add a button to start screenshot capture
     
     
     root.mainloop()
+    
+def screenshot(root):
+    print("button clicked")
+    screenshot = ImageGraber.grabScreenshot()
+    screenshot = Image.fromarray(screenshot)
+    image = ImageTk.PhotoImage(screenshot)
+    label = tk.Label(root,image=image)
+    label.image = image
+    label.pack()
+
+    
 
 
 #start program
 if __name__ == "__main__":
     main_Gui()
+    
+
